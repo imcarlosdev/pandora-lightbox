33f1cfe3fcd9d05a84ed415e14ad3ae4d26b01f3
Carlos Maldonado <karlozm@gmail.com>
2018-11-02 11:54:38 -0600

    major updates to old code, this is a new stable version

--- source/pandora.jquery.js
+++ source/pandora.jquery.js
@@ -1,55 +1,112 @@
 /*
 PANDORA LIGHTBOX V1.0
-THIS WORK IS UNDER MIT LICENCE http://opensource.org/licenses/MIT 
-With tons of love by @choquo - Release Date 11-05-2014 
+License: MIT
+Author: Carlos Maldonado @choquo
+Release date: 11-05-2014 
+Last update: 31-07-2017
 */
 
-var transitionSpeed = 500;
-var offsetContent = 100; /*Margin top and botton for content*/
-var loadingPath = 'source/loading.gif'; /*The path for loading gif*/
+var pandoraTransitionSpeed = 300;
+var pdLoadingPath = 'source/loading.svg'; /*The path for loading gif*/
+
+function closePandoraLightbox(){
+	$("#pandora-lb-wrapper").fadeOut(pandoraTransitionSpeed, function(){ 
+		$("#pandora-lb-wrapper").remove(); 
+		$("body").css({"overflow":"visible"}); 
+	}); 
+}
 
 $(function(){
 
 	$(".pandora-lightbox").click(function(){
 
-		/*Reset all opened lightbox*/
+		var uri = $(this).attr('href');
+		console.log(uri);
+
+		//Reset all opened lightbox
 		$("#pandora-lb-wrapper").remove();
 
-		/*Prepare lightbox layout*/
-		$("body").prepend('<div id="pandora-lb-wrapper"><div id="pandora-lb-content"><div id="pandora-lb-content-inner"><div id="pandora-lb-loading"><img src="'+loadingPath+'"></div></div></div> <script>$(function(){ $("#pandora-lb-content-inner").click(function(e){ e.stopPropagation(); }); $("#pandora-lb-wrapper, #pandora-lb-content").click(function(e){ $("#pandora-lb-wrapper").fadeOut('+transitionSpeed+', function(){ $("#pandora-lb-wrapper").remove(); $("body").css({"overflow":"visible"}); }); }); });</script></div>');
+		//Prepare lightbox layout
+		$("body").prepend(''+
+			'<div id="pandora-lb-wrapper" style="opacity: 0;">'+
+				'<div id="pandora-lb-content">'+
+					'<div id="pandora-lb-content-inner">'+
+						'<div id="pandora-lb-loading">'+
+							'<img src="'+pdLoadingPath+'">'+
+						'</div>'+
+					'</div>'+
+				'</div>'+
+			'</div>'
+		);
 
-		/*Load content*/
-		var uri = $(this).attr('href');
+		//Show the background of the lightbox
+		$("#pandora-lb-wrapper").animate({opacity: 1}, pandoraTransitionSpeed, function() {
+
+			setTimeout(function(){
 		
-		$("body").css({'overflow': 'hidden'});
+				//On animation complet Load content
 
-		/*Documents*/
-		if(/.php|.htm|.html/.test(uri)){
-			var req = $.get(uri, function(content){
-						$("#pandora-lb-wrapper").fadeIn(transitionSpeed);
-						$("#pandora-lb-content-inner").html(content).animate({'margin-top': offsetContent, 'margin-bottom': offsetContent}, transitionSpeed);
-						
-					});
-			$("#pandora-lb-wrapper").bind("click", function(){ 	req.abort(); });
+				//Begin load content
+				$("body").css({'overflow': 'hidden'});
 
-			return false;
-		}
+				//*** PDF
+				if( /\.pdf/.test(uri) ){
+					var content = '<iframe allowfullscreen webkitallowfullscreen id="pandora-lb-iframe" style="border: none; width: 100%;" src="source/vendors/ViewerJS/#../../../'+uri+'"></iframe>';
+					$("#pandora-lb-content-inner").html(content);
+					$("#pandora-lb-wrapper").fadeIn(pandoraTransitionSpeed);
+					$("#pandora-lb-iframe").height( parseInt($("#pandora-lb-wrapper").height())-60 );
+					return false;
+				}
 
-		/*Images*/
-		if(/.png|.jpg/.test(uri)){
-			var content = '<img src="'+uri+'">';
-			var req = $.get(uri, function(){
-						$("#pandora-lb-content-inner").html(content).animate({'margin-top': offsetContent, 'margin-bottom': offsetContent}, transitionSpeed);
-						$("#pandora-lb-wrapper").fadeIn(transitionSpeed);
-					  });
-			
-			$("#pandora-lb-wrapper").bind("click", function(){ 	req.abort(); });
+				//*** Images
+				if( /\.png|\.jpg|\.jpeg|\.bmp|\.gif|\.svg/.test(uri) ){
+					var content = '<img class="pandora-lb-image" src="'+uri+'">';
+					$("#pandora-lb-content-inner").html(content);
+					$("#pandora-lb-wrapper").fadeIn(pandoraTransitionSpeed);
+					return false;
+				}
 
-			return false;
-		}
+				//*** External pages
+				if( /http\:|https\:/.test(uri) ){
+					var content = '<iframe allowfullscreen webkitallowfullscreen id="pandora-lb-iframe" style="border: none; width: 100%;" src="'+uri+'"></iframe>';
+					$("#pandora-lb-content-inner").html(content);
+					$("#pandora-lb-wrapper").fadeIn(pandoraTransitionSpeed);
+					$("#pandora-lb-iframe").height( parseInt($("#pandora-lb-wrapper").height())-60 );
+					return false;
+				}
+
+				//*** HTML PHP - Documents (internal)
+				if( /\.htm|\.html|\.php/.test(uri) ){
+					var req  =	$.get(uri, function(content){
+									$("#pandora-lb-content-inner").html(content);
+									$("#pandora-lb-wrapper").fadeIn(pandoraTransitionSpeed);
+							  	});
+					$("#pandora-lb-wrapper").bind("click", function(){ 	req.abort(); });
+					return false;
+				}
+			},400);
+
+		});
 
 		return false;
 
 	});
 
+
+	
+	//Close Lightbox
+	$("body").on("click", "#pandora-lb-content-inner *", function(e){ 
+		e.stopPropagation(); 
+	});
+	$("body").on("click", "#pandora-lb-wrapper, #pandora-lb-content, #pandora-lb-content-inner", function(e){
+		closePandoraLightbox();
+	});
+
+});
+	
+//Close Lightbox on press ESC
+$(document).keyup(function(e) {
+     if (e.keyCode == 27) {
+        closePandoraLightbox();
+    }
 });
